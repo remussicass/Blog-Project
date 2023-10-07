f7c0fa2d792631704bc0dffe5762a2985cbb35ae
Remus <remi_rockz@yahoo.com>
2023-10-07 16:55:28 +0300

    add psycopg2

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from sqlalchemy.exc import IntegrityError
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
